357da52b4cafbc7e4dc3e7524b1e1f6a95360dc5
yange <yange@apple.com>
2016-09-14 16:58:53 +0800

    Commit 8-3: Only allow changing certain fields of posts.

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -19,7 +19,7 @@ Template.postSubmit.events({
   'click .delete': function(e){
 	e.preventDefault();
 
-	if (confirm("Delete this post?")){
+	if(confirm("Delete this post?")){
 		var currentPostId = this._id;
 		Posts.remove(currentPostId);
 		Router.go('postsList');
