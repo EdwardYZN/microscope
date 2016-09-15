302062424b61fd38fc3c6247f541386d2f9b4f20
yange <yange@apple.com>
2016-09-15 10:19:52 +0800

    Commit 8-3: corrected errors

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,16 +1,18 @@
-Template.postSubmit.events({
+Template.postEdit.events({
   'submit form': function(e){
   	e.preventDefault();
 	
-	var post={
+	var currentPostId=this._id;
+
+	var postProperties={
 		url:$(e.target).find('[name=url]').val(),
 		title:$(e.target).find('[name=title]').val()	
-	};
+	}
 	
 	Posts.update(currentPostId, {$set: postProperties}, function(error){
 		if (error) {
 			alert(error.reason);
-		} else{
+		} else {
 			Router.go('postPage',{_id: currentPostId});
 		}
 	});
@@ -18,7 +20,7 @@ Template.postSubmit.events({
 
   'click .delete': function(e){
 	e.preventDefault();
-
+	
 	if(confirm("Delete this post?")){
 		var currentPostId = this._id;
 		Posts.remove(currentPostId);
