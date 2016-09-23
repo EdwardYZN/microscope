a009bb64e515a493b3dd698a4077258eefbd7309
yange <yange@apple.com>
2016-09-23 14:11:39 +0800

    Commit 17-1: Added Intercom code snippet.

---
+++ client/main.js
@@ -0,0 +1,6 @@
+window.intercomSettings = {
+    app_id: "oacjoxyi",
+    name: "John Doe", // Full name
+    email: "john.doe@example.com", // Email address
+    created_at: 1312182000 // Signup date as a Unix timestamp
+};
